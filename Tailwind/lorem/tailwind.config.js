1160afb6377f3de72968a22ccbdc28d6e4e45279
ManuelCA12 <cervanteshector57@gmail.com>
2024-05-30 21:29:40 -0600

    Add files via upload

---
+++ Tailwind/lorem/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mimi: '#1fb9ff',
+        purple: '#7e5bef',
+        pink: '#ff49db',
+      },
+      },
+    },
+  plugins:[],
+}
